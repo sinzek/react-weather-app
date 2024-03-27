f1c99b2e72cef7634bad2194fb2d466963166a95
Chase Brock <chase.p.brock@gmail.com>
2024-03-27 01:03:10 -0500

    formatting for current-weather

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,17 +1,25 @@
-import "./current-weather.css"
+import "./current-weather.css";
 
 const CurrentWeather = () => {
-    return (
-        <div className="weather">
-            <div className="top">
-                <div>
-                    <p className="city">Belgrade</p>
-                    <p className="weather-description">Sunny</p>
-                </div>
-                <img alt="weather" className="weather-icon" src="icons/sunny-cloudy-rainy.png" />
-            </div>
+  return (
+    <div className="weather">
+        <div className="items">
+            
         </div>
-    )
-}
+      <div className="city-name">
+        <p className="city-text">Belgrade</p>
+      </div>
+      <div className="info">
+        <p className="temperature">18°</p>
+        <p className="weather-description">Sunny</p>
+      </div>
+      <img
+        alt="weather"
+        className="weather-icon"
+        src="icons/sunny-cloudy-rainy.png"
+      />
+    </div>
+  );
+};
 
 export default CurrentWeather;
